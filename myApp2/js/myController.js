eb70a610130dd14f0ba8d02ccf3717d05d3afff5
Anendil <duvalflorian14@gmail.com>
2016-02-09 10:27:13 +0100

    push3

--- myApp2/js/myController.js
+++ myApp2/js/myController.js
@@ -1,25 +1,41 @@
-angular.module("myApp").controller("myController",[function(){
-    this.messageNote="";
-    this.showinfo = false;
+angular.module("myApp2").controller("myController",[function(){
 
-    this.count=function(){
-        return 100 - this.messageNote.length;
-    };
+    this.etatButton="";
 
-    this.color=function(){
-        if(100 - this.messageNote.length < 20){
-            return 'danger';
+    this.services = [
+        {
+            name: 'Web Development',
+            price: 300,
+            active:true
+        },{
+            name: 'Design',
+            price: 400,
+            active:false
+        },{
+            name: 'Integration',
+            price: 250,
+            active:false
+        },{
+            name: 'Formation',
+            price: 220,
+            active:false
         }
-        else if(100 - this.messageNote.length < 50){
-            return 'warning';
-        }
-        else
-            return 'info';
-    };
+    ];
 
-    this.clear=function(){
-        this.showinfo = true;
-        this.messageNote="";
+    this.isActive= function(boolean){
+        if(boolean) {
+            this.etatButton = "btn btn-primary";
+            return this.etatButton;
+        }
+        else{
+            this.etatButton="btn btn-default";
+            return this.etatButton;
+        }
+    }
+
+    this.toggleActive = function(s){
+        isActive(s.active);
+        s.active =  !s.active;
     }
 
 }]);
